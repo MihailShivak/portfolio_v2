224516c77a79ea1d8e62e81b6a32020ceb459d57
Mihail Shivak <shivakmisha@gmail.com>
2022-08-09 15:44:44 +0300

    upd: 9.08

--- back/middleware/authMiddleware.js
+++ back/middleware/authMiddleware.js
@@ -6,7 +6,7 @@ module.exports = function(req,res,next){
       next()
    }
    try {
-      const token = req.headers.authorization.split(' ')[1] // Bearer adsakdsasdasdjaksd
+      const token = req.headers.authorization.split(' ')[1]
       if(!token){
          return res.status(401).json({message: 'NE AVTORIZOVAN'})
       }
